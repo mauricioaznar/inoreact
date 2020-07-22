77ed98269876eee310a04166dc165cd0ef9c5fa5
Maguas Tupaguas <mauricioaznar94@gmail.com>
2020-07-22 00:09:32 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import combinedReducers from './store/index'
 
-
 const store = createStore(combinedReducers, {}, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 
