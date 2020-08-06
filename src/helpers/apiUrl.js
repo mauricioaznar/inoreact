2c66e805cb078e8a6d044968d670e1602ef981eb
mauricioaznar <maurisio01>
2020-08-06 15:01:05 -0500

    refactoring useFetch

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -1,3 +1,4 @@
-const apiUrl = process.env.REACT_APP_BASE_URL
+const apiUrl = process.env.NODE_ENV === 'development' ?
+  'http://inoserver.test/api/' : process.env.REACT_APP_BASE_URL
 
 export default apiUrl
