8a217f66e8d7a4137f3f19c437bf412f57377d31
mauricioaznar <maurisio01>
2020-08-06 14:27:09 -0500

    refactoring useFetch

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -2,5 +2,6 @@ const apiUrl = process.env.NODE_ENV === 'development' ?
   'http://inoserver.test/api/' : 'https://babytester.grupoinopack.com/api/'
 
 console.log(process.env)
+console.log(process.env)
 
 export default apiUrl
