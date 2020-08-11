31162e61fef57b5cdd6f7be458e6b5cf088825fd
Maguas Tupaguas <mauricioaznar94@gmail.com>
2020-08-10 21:42:35 -0500

    expense estimations

---
+++ src/components/ui/ExpensesVsSales.js
@@ -0,0 +1,35 @@
+import React, { PureComponent } from 'react';
+import {
+  LineChart, Line, XAxis, YAxis, Legend, CartesianGrid, Tooltip, ResponsiveContainer,
+} from 'recharts';
+import mdColors from '../../helpers/colors'
+import AreaChart from 'recharts/lib/chart/AreaChart'
+import Area from 'recharts/lib/cartesian/Area'
+
+export default class ExpensesVsSales extends PureComponent {
+  render() {
+    if (this.props.sales && this.props.expenses) {
+      return (
+        <div style={{ width: '100%', height: 400 }}>
+          <ResponsiveContainer>
+            <AreaChart
+              height={300}
+              data={this.props.data}
+              margin={{
+                top: 10, bottom: 20,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey={this.props.xDataKey} angle={-45} textAnchor="end" interval={2}/>
+              <YAxis />
+              <Legend verticalAlign="top" iconType="square" />
+              {this.props.dataKeys.map((dataKey, index) => {
+                return <Area key={dataKey} type="monotone" dataKey={dataKey} stroke={mdColors[(index - 1) * 20]} fill={mdColors[(index - 1) * 20]} />
+              })}
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+      );
+    }
+  }
+}
