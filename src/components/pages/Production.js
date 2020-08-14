e5d290707c23017259c8dc0773f6b09050d43a3d
mauricioaznar <maurisio01>
2020-08-14 15:26:43 -0500

    data table testing

--- src/components/pages/Production.js
+++ src/components/pages/Production.js
@@ -45,23 +45,23 @@ const dateFormat = 'YYYY-MM-DD'
 
 
 const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} color={'action'} fontSize={'small'}/>),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} color={'action'}  fontSize={'small'}/>),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} color={'action'}  fontSize={'small'}/>)
 };
 
 const useFetch = (url) => {
