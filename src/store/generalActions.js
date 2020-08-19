95fdfee98d8e55a05293c8485662f55ff03f75a1
mauricioaznar <maurisio01>
2020-08-19 16:40:18 -0500

    performance upgrade biaches

--- src/store/generalActions.js
+++ src/store/generalActions.js
@@ -23,19 +23,20 @@ export const getApiEntities = () => {
     dispatch(setAreEntitiesLoading())
     return axios.all([
       axios.get(apiUrl + 'stats/inventory', {headers: {...authHeader()}}),
-      axios.get(apiUrl + 'expenseCategory/list?paginate=false', {headers: {...authHeader()}}),
       axios.get(apiUrl + 'stats/dependentEntities', {headers: {...authHeader()}}),
-      axios.get(apiUrl + 'material/list?paginate=false', {headers: {...authHeader()}}),
-      axios.get(apiUrl + 'productType/list?paginate=false', {headers: {...authHeader()}}),
-      axios.get(apiUrl + 'supplier/list?paginate=false', {headers: {...authHeader()}})
     ]).then(result => {
       // const orderProductions = result[0].data.data[0]
       const inventory = result[0].data.data
-      const expenseCategories = result[1].data.data
-      const {expense_subcategories: expenseSubcategories, machines, branches, expense_type: expenseTypes} = result[2].data.data
-      const materials = result[3].data.data
-      const productTypes = result[4].data.data
-      const suppliers = result[5].data.data
+      const {
+        expense_subcategories: expenseSubcategories,
+        machines,
+        branches,
+        expense_type: expenseTypes,
+        expense_categories: expenseCategories,
+        suppliers,
+        product_type: productTypes,
+        materials
+      } = result[1].data.data
       // dispatch(setOrderProductions(orderProductions))
       dispatch(setInventory(inventory))
       dispatch(setBranches(branches))
