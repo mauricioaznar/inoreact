242f7316941fda8e35e21d0e8b7f1eeea4ee9149
Maguas Tupaguas <mauricioaznar94@gmail.com>
2020-09-01 23:14:06 -0500

    Wrapper component on material ui element

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
 import {connect} from 'react-redux'
+import {loginUser} from './store/authActions'
 
 
 
 function App(props) {
+  //Correct permanent session logic with dates, token invalidation and token creation
+  // Could check on intervals to execute this function
+  React.useEffect(() => {
+    const {email, password} = window.localStorage.getItem('loginForm') ? JSON.parse(window.localStorage.getItem('loginForm')) : {}
+    const initialEmail = email || ''
+    const initialPassword = password || ''
+    if (initialEmail !== '' && initialPassword !== '') {
+      props.loginUser(initialEmail, initialPassword)
+    }
+  }, [])
 
   return (
     <div className="Auth">
@@ -21,5 +32,12 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
+const mapDispatchToProps = (dispatch, getState) => {
+  return {
+    loginUser: (email, password) => {dispatch(loginUser(email, password))}
+  }
+}
 
-export default connect(mapStateToProps, null)(App);
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
