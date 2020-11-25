6ee07b3666111f8464fab17562cb05b4c885db51
mauricioaznar <maurisio01>
2020-11-25 16:24:30 -0600

    Por producir

--- src/helpers/imageUrl.js
+++ src/helpers/imageUrl.js
@@ -1,4 +1,5 @@
+
 const apiUrl = process.env.NODE_ENV === 'development' ?
-  'http://inoserver.test/api/' : process.env.REACT_APP_BASE_URL + 'api/'
+  'http://inoserver.test/' : process.env.REACT_APP_BASE_URL
 
 export default apiUrl
